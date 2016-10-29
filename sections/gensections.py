27e2396037f90cf6e681f18ee6d8bcc9d2922d42
cerea1killer <cerea1killer@tackitt.net>
2016-10-29 03:35:41 +0000

    Fix gensections
    make clean really cleans now
    make also runs gensections and getfwimg as needed

--- sections/gensections.py
+++ sections/gensections.py
@@ -1,8 +1,8 @@
 with open('../bin/fw.img.full.bin', 'rb') as infw:
 	with open('0x5100000.bin', 'wb') as fout:
 		infw.seek(0x89F1C, 0)
-		fout.write(f.read(0x15D6C))
+		fout.write(infw.read(0x15D6C))
 
 	with open('0x8140000.bin', 'wb') as fout:
 		infw.seek(0xB4C88, 0)
-		fout.write(f.read(0x2478))
+		fout.write(infw.read(0x2478))
